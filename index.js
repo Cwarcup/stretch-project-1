4ed251d7cc3acbf025b4fdc3be55ae25e41c9269
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-05-25 10:57:47 -0700

    bar chart height

--- index.js
+++ index.js
@@ -23,21 +23,27 @@ const createBarChart = (data, options, element) => {
   $(element).add(data.toString());
 
   let numOfBars = data.length;
+  let maxValue = Math.max(...data);
+  console.log(maxValue);
 
   for (let i = 0; i < numOfBars; i++) {
-    console.log("barcharty height" + $("#barChart").height());
-    console.log(data[i]);
-    let percentHeight =
-      ($("#barChart").height() / $("#barChart").height()) * data[i];
-    console.log(percentHeight);
+    let containerPx = $("#barChart").height();
+    console.log("container height: " + containerPx);
+    let maxBarHeight =
+      (Math.floor($("#barChart").height()) * (maxValue * 0.9)) / 100;
+    console.log("maxBarHeight: " + maxBarHeight);
+
+    let barHeightRelToMax = Math.floor((maxBarHeight * data[i]) / 100);
+    console.log("barHeightRelToMax: " + barHeightRelToMax);
+
     $(".xAxisContainer").append(
       `
-      <div class='bar' id='${data[i]}' value='${data[i]}' style='height: ${percentHeight}%;'>${data[i]}</div>
+      <div class='bar' id='${data[i]}' value='${data[i]}' style='height: ${barHeightRelToMax}px;'>${data[i]}</div>
       `
     );
   }
 };
-let testData = [5, 10, 15, 20, 50];
+let testData = [5, 10, 15, 75, 20, 50, 99];
 let testOptions = {
   width: "80%",
   height: "400px",
