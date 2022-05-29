9acc22c0c89507da0579c6230a03a4e5d693cebe
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-05-29 09:11:18 -0700

    border for single graph

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const createBarChart = (data, options, element) => {
 
   $("#grid-chart").css("grid-column-gap", `${gapSize}`);
 
+  // bar border color
+
   // add bars to gridchart between 2 / 3
   let colStart = 2;
   let colEnd = 3;
@@ -35,6 +37,8 @@ const createBarChart = (data, options, element) => {
       background: ${barColor}; 
       grid-column: ${colStart} / ${colEnd}; 
       grid-row: ${startingRow - data[i]} / ${maxValue + 2};
+      border: 1px solid ${LightenDarkenColor(barColor, -40)};
+      border-bottom: none;
       '></div>
       <div class='dataLabel' 
       style=' 
@@ -70,6 +74,33 @@ const createBarChart = (data, options, element) => {
   // end of initial setup fn
 };
 
+// dynamically change border color for a given hex
+function LightenDarkenColor(col, amt) {
+  let usePound = false;
+  if (col[0] == "#") {
+    col = col.slice(1);
+    usePound = true;
+  }
+
+  let num = parseInt(col, 16);
+
+  let r = (num >> 16) + amt;
+
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+
+  let b = ((num >> 8) & 0x00ff) + amt;
+
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+
+  let g = (num & 0x0000ff) + amt;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+}
 // initialize default single bar chart
 createBarChart(defaultData, defaultOptions, defaultElement);
 
